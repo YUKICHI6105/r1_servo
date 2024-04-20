21819ae771e51ecd65a9b0ebef7fc13fad6aad8a
yukichi605 <107849799+YUKICHI6105@users.noreply.github.com>
2024-04-20 23:12:20 +0900

    update

--- include/servo/servo_node.hpp
+++ include/servo/servo_node.hpp
@@ -9,9 +9,9 @@ struct ServoArray{
   uint16_t lowerValue[4] = {};//サーボの後半
   std::vector<std::string> mode = {"Toggle","Toggle","Toggle","Toggle","Toggle","Toggle","Toggle","Toggle"};//ToggleかNomalか
   // std::vector<int64_t> button(8,0);//ボタン割り当て
-  std::vector<int64_t> button = {2,2,2,2,2,2,2,2};
+  std::vector<int64_t> button = {2,2,2,4,1,1,1,1};
   uint8_t countvalve1[8] = {};//カウントバルブの状態
   uint8_t preButton[8] = {};//ボタンの前の状態
-  std::vector<int64_t> ccr1 = {0,50000,20000,0,0,0,0,0};//CCR1の値
-  std::vector<int64_t> ccr2 = {0,20000,50000,0,0,0,0,0};//CCR2の値
+  std::vector<int64_t> ccr1 = {0,40000,20000,17000,30000,30000,18000,18000};//CCR1の値
+  std::vector<int64_t> ccr2 = {0,20000,40000,32000,18000,18000,30000,30000};//CCR2の値
 };
